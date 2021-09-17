ff0832507a82c948e629327896ac4f3947ec130e
Manish <m9shdey@gmail.com>
2021-09-17 16:01:57 +0530

    Modified Discover.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 export default {
   api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
-  }
-}
+    baseUrl: "https://api.spotify.com/v1",
+    authUrl: "https://accounts.spotify.com/api/token",
+    clientId: "8a71737662b34529baad288c6a6f2118",
+    clientSecret: "ed8d17ddfc294ed59a226467368767d6",
+    AuthToken:
+      "BQCEwMIO1xdI2l8-w7dEHexKCV6FP3lsO4mP2-eO3FYE8o76Z_IW8sPZotkkDo_ZdQOF8pucRAKgNl4XISb5aHjh7Kh1AfUt0iQrNoNv7wjHd4XuV0he5WDtR6Yg3bXNVgyUo0BJtZRp-09E-BwRmml0K7zI98WDO0g",
+  },
+};
--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import "../styles/_discover.scss";
+import axios from "axios";
+import api from "../../../config";
+
+const API_ROOT = api.api.baseUrl; // extracting baseUrl of api from api object
+localStorage.setItem("token", api.api.AuthToken); // storing token in local storage
 
 export default class Discover extends Component {
   constructor() {
@@ -9,18 +14,97 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
     };
   }
 
+  //calling functions in componentDidMount which results in API calls
+
+  componentDidMount() {
+    this.fetchReleased();
+    this.fetchFeaturedPlaylist();
+    this.fetchCategories();
+  }
+
+  // function for fetching released albums
+
+  fetchReleased = async () => {
+    let token = localStorage.getItem("token"); //getting token from localStorage
+
+    // using axios library to fetch data
+    const response = await axios({
+      method: "get",
+      url: `${API_ROOT}/browse/new-releases?country=IN&limit=50&offset=6`, //passing the value of API_ROOT using  string literal
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`, // Passing Authentication token that we got from localStorage
+      },
+    });
+
+    const data = response.data;
+
+    //setting the data to state using setState
+    this.setState({
+      newReleases: data.albums.items,
+    });
+  };
+
+  //function for fetching featured playlist
+
+  fetchFeaturedPlaylist = async () => {
+    let token = localStorage.getItem("token"); //getting token from localStorage
+    const response = await axios({
+      method: "get",
+      url: `${API_ROOT}/browse/featured-playlists?country=IN&limit=50&offset=6`, //passing the value of API_ROOT using  string literal
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`, // Passing Authentication token that we got from localStorage
+      },
+    });
+    const data = response.data;
+
+    //setting the data to state using setState
+    this.setState({
+      playlists: data.playlists.items,
+    });
+  };
+
+  //function for fetching categories
+
+  fetchCategories = async () => {
+    let token = localStorage.getItem("token"); //getting token from localStorage
+    const response = await axios({
+      method: "get",
+      url: `${API_ROOT}/browse/categories`, //passing the value of API_ROOT using  string literal
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`, // Passing Authentication token that we got from localStorage
+      },
+    });
+
+    const data = response.data;
+
+    //setting the data to state using setState
+    this.setState({
+      categories: data.categories.items,
+    });
+  };
+
   render() {
     const { newReleases, playlists, categories } = this.state;
-
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock text="BROWSE" id="browse" data={categories} />
       </div>
     );
   }
